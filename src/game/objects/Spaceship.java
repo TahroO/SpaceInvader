aaf0546fde6a0d3609baa836514307dcd12db084
Steffen <steffen@13-bits.de>
2023-01-28 23:28:33 +0100

    spaceship doc

--- src/game/objects/Spaceship.java
+++ src/game/objects/Spaceship.java
@@ -4,21 +4,34 @@ import game.GameObject;
 import game.GameSurface;
 
 import java.awt.*;
-import java.util.Collection;
 
+/**
+ * Represents a spaceship.
+ */
 public class Spaceship extends GameObject {
-
     public static final int SHIP_WIDTH = 80;
     public static final int SHIP_HEIGHT = 30;
-    protected int dir;
-    private int vx;
+    private final int dir;
+    private final int vx;
 
+    /**
+     * Creates a new SpaceShip instance.
+     * @param x
+     * @param y
+     * @param vx
+     * @param dir
+     */
     public Spaceship(int x, int y, int vx, int dir) {
-        super (x, y, SHIP_WIDTH, SHIP_HEIGHT);
+        super(x, y, SHIP_WIDTH, SHIP_HEIGHT);
         this.vx = vx;
         this.dir = dir;
     }
 
+    /**
+     * Returns a new SpaceShip instance.
+     * @param dir Direction (either -1 for rtl or 1 for ltr).
+     * @return A SpaceShip object.
+     */
     public static Spaceship startShip(int dir) {
         int y = 35;
         int vx = 133;
@@ -29,6 +42,10 @@ public class Spaceship extends GameObject {
         return new Spaceship(x, y, vx, dir);
     }
 
+    /**
+     * Gets spaceship's current direction.
+     * @return Either -1 when direction is right to left or 1 for left to right.
+     */
     public int getDirection() {
         return dir;
     }
@@ -44,4 +61,5 @@ public class Spaceship extends GameObject {
         g2d.setColor(Color.CYAN);
         g2d.fillOval(x, y, SHIP_WIDTH, SHIP_HEIGHT);
     }
+
 }
